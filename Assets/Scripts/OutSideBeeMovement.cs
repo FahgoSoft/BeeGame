63f6791ea8a6ee615d9a92d1c4ac521a0b2145ff
ben.fison <ben.fison@gmail.com>
2019-01-08 22:00:09 +0000

    Game added

---
+++ Assets/Scripts/OutSideBeeMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OutSideBeeMovement : MonoBehaviour
+{
+    public bool inside;
+    private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    private bool walking;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+        navMeshAgent = GetComponent<NavMeshAgent> ();
+        count = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inside)
+        {
+            transform.Rotate(new Vector3(90, 0, 0));
+        }
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                // if (hit.collider.CompareTag("Enemy"))
+                // {
+                //     targetedEnemy = hit.transform;
+                //     enemyClicked = true;
+                // }
+
+                // else
+                // {
+                    walking = true;
+                    // enemyClicked = false;
+                    navMeshAgent.destination = hit.point;
+                    navMeshAgent.isStopped = false;
+                // }
+            }
+        }
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Pollen"))
+        {
+            other.gameObject.SetActive (false);
+            count+=1;
+            // SetCountText();
+        }
+    }
+}
