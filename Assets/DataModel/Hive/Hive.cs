0d56a4f4427165296edc065e428c29c2449a9c55
ben.fison <ben.fison@gmail.com>
2019-02-05 22:14:21 +0000

    Added data model for hive

---
+++ Assets/DataModel/Hive/Hive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hive 
+{
+    //Properties
+    int Width;
+    int Height;
+
+    //Resources
+    int HoneyCount=0;
+
+    //Rooms
+    Rooms[] rooms;
+
+    public Hive()
+    {
+        Width = 3;
+        Height = 3;
+
+    }
+
+}
---
+++ Assets/DataModel/Hive/Rooms.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms
+{
+    //Properties
+    int ID;
+    string Label;
+    int X, Y;
+    RoomTypes Type;
+
+    //Enums
+    enum RoomTypes {QueensChamber,Hatchery,Stockpile,Empty};
+
+    //Todo: Connections/blockages
+
+    public Rooms(int id, int x, int y)
+    {
+        this.ID = id;
+        this.X = x;
+        this.Y = y;
+        this.Type = RoomTypes.Empty;
+    }
+}
