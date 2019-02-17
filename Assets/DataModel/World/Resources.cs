6f21bcb330099d15064826ddee8ead124d709c81
FahgoSoft <sarahfison977@gmail.com>
2019-02-17 19:51:47 +0000

    update

---
+++ Assets/DataModel/World/Resources.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjects
+{
+    int ID;
+    int X, Z;
+    ResourceType Type;
+
+    public enum ResourceType {Flowers, Trees, PollenBlobs}
+
+    public MapObjects( int id, int x, int z, ResourceType type)
+    {
+        this.ID = id;
+        this.X = x;
+        this.Z = z;
+        this.Type = type;
+
+    }
+
+
+    
+}
